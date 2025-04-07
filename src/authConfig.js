4655b1924801222b1279ff97fcb9fa6b0328d121
lucasfini <lucasfiniello@gmail.com>
2025-04-07 12:13:37 -0400

    updated auth

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     auth: {
       clientId: "7d2c5fe8-85f8-424b-8196-e705e6a864ca", // Replace with your actual client ID if different
       authority: "https://login.microsoftonline.com/52e54255-72b0-4649-951a-2de4c170009d", // McMaster tenant
-      redirectUri: "http://localhost:3000",
+      redirectUri: "http://159.203.59.163",
     },
     cache: {
       cacheLocation: "sessionStorage",
