08b6af0f49e48a94b56c9cdb53964d96b384b334
lucasfini <lucasfiniello@gmail.com>
2025-04-08 13:58:33 -0400

    login activated

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,8 +3,8 @@ export const msalConfig = {
   auth: {
     clientId: "7d2c5fe8-85f8-424b-8196-e705e6a864ca",
     authority: "https://login.microsoftonline.com/52e54255-72b0-4649-951a-2de4c170009d",
-    redirectUri: "http://localhost:3000",
-    postLogoutRedirectUri: "http://159.203.59.163"
+    redirectUri: "https://upload.msuit.ca", // Your domain
+    postLogoutRedirectUri: "https://upload.msuit.ca" // Updated to use your domain instead of IP
   },
   cache: {
     cacheLocation: "sessionStorage",
